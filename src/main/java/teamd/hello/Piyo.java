8d5f7b3d13ce09bee9699a87742e7b8d60023fac
takahitosuzukiniandc <takahito_suzuki@niandc.co.jp>
2018-02-20 17:38:02 +0900

    test

---
+++ src/main/java/teamd/hello/Piyo.java
@@ -0,0 +1,25 @@
+package teamd.hello;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Piyo 
+{
+	private Map<String, String> idPassMap = new HashMap<String, String>()
+	{
+		{put("hoyo", "hoyoyo");}
+		{put("hoge", "piyo");}
+		{put("hoyo", "hoyoyo");}
+		{put("oyo", "oyoyo");}
+	};
+	
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+	
+	public String getPassword(String userId) {
+		return idPassMap.get(userId);
+		//return null;
+	}
+}
